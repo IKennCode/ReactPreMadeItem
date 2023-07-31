340aebb9a1a4506d5269ed7f977feb931b06f6ec
Kenneth Malabriga Dimalibot <138239593+IKennCode@users.noreply.github.com>
2023-08-01 00:01:25 +0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,21 @@
+ import { Products } from './components/products';
+ import contents from './content';
+
+ export default function App() {
+     return(
+            <div className='App'>
+                {contents.map(contents => (
+                    <Products 
+                        key={contents.id}
+                        image={contents.image}
+                        name={contents.name}
+                        sale={contents.sale}
+                        price={contents.price}
+                        totalSales={contents.totalSales}
+                        timeLeft={contents.timeLeft}
+                        rating={contents.rating}
+                    />
+                ))}
+            </div>
+     )
+ }
